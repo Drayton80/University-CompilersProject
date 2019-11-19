dfe597d5f3b7352c42e57b8d8250ccbf5aab12c5
Drayton80 <drayton80@hotmail.com>
2019-11-19 17:31:06 -0300

    2.1 - Variable Declaration and Procedures Declaration

--- src/SyntacticAnalyzer.py
+++ src/SyntacticAnalyzer.py
@@ -18,12 +18,112 @@ class SyntacticAnalyzer:
 
     def _variables_declaration(self):
         if self._current_value['token'] == 'var':
-            self._list_variable_declaration()
+            self._next_value()
+            self._list_variable_declaration1()
+        else:
+            return None
 
-    def _list_variable_declaration(self):
-        pass
+    def _list_variable_declaration1(self):
+        self._list_identifiers1()
 
-    def _procedures_declaration(self):
+        if self._next_value()['token'] == ':':
+            self._next_value()
+            self._type()
+            
+            if self._next_value()['token'] == ';':   
+                self._next_value()
+                self._list_variable_declaration2()
+            else:
+                print("ERRO na Linha:", self._current_value['line'])
+        else:
+            print("ERRO na Linha:", self._current_value['line'])
+
+    def _list_variable_declaration2(self):
+        if self._current_value['class'] == 'Identificador':
+            self._list_identifiers1()
+            
+            if self._next_value()['token'] == ':':
+                self._next_value()
+                self._type()
+                
+                if self._next_value()['token'] == ';':
+                    self._next_value()
+                    self._list_variable_declaration2()
+                else:
+                    print("ERRO na Linha:", self._current_value['line'])
+            else:
+                print("ERRO na Linha:", self._current_value['line'])
+        else:
+            return None
+
+    def _list_identifiers1(self):
+        if self._current_value['class'] == 'Identificador':
+            self._next_value()
+            self._list_identifiers2()
+        else:
+            print("ERRO na Linha:", self._current_value['line'])
+
+    def _list_identifiers2(self):
+        if self._current_value['token'] == ',':
+            if self._current_value['class'] == 'Identificador':
+                self._next_value()
+                self._list_identifiers2()
+            else:
+                print("ERRO na Linha:", self._current_value['line'])
+        else:
+            return None
+
+    def _type(self):
+        if self._current_value['class'] == 'Número inteiro':
+            return None
+        elif self._current_value['class'] == 'Número real':
+            return None
+        elif self._current_value['class'] == 'Booleano':
+            return None
+        else:
+            print("ERRO na Linha:", self._current_value['line'])
+
+    def _list_procedures_declaration1(self):
+        self._procedure_declaration()
+
+        if self._next_value()['token'] == ';':
+            self._list_procedures_declaration2()
+        else:
+            print("ERRO na Linha:", self._current_value['line'])
+
+    def _list_procedures_declaration2(self):
+        if self._next_value()['token'] == 'procedure':
+            self._procedure_declaration()
+
+            if self._next_value()['token'] == ';':
+                self._list_procedures_declaration2()
+            else:
+                print("ERRO na Linha:", self._current_value['line'])
+        else:
+            return None
+        
+    
+    def _procedure_declaration(self):
+        if self._current_value['token'] == 'procedure':
+            if self._next_value()['class'] == "Identificador":
+                self._next_value()
+                self._aurgment()
+                
+                if self._next_value()['token'] == ';':
+                    self._next_value()
+                    self._variables_declaration()
+                    
+                    self._next_value()
+                    self._list_procedures_declaration1()
+                    
+                    self._next_value()
+                    self._compound_statement()
+                else:
+                    print("ERRO na Linha:", self._current_value['line'])
+            else:
+                print("ERRO na Linha:", self._current_value['line'])
+
+    def _aurgment(self):
         pass
 
     def _compound_statement(self):
@@ -35,9 +135,13 @@ class SyntacticAnalyzer:
                 if self._next_value()['token'] == ';':
                     self._next_value()
                     self._variables_declaration()
-                    self._procedures_declaration()
+                    
+                    self._next_value()
+                    self._list_procedures_declaration1()
+                    
+                    self._next_value()
                     self._compound_statement()
 
                     if self._next_value()['token'] != '.':
-                        print('ERRO')
+                        print("ERRO na Linha:", self._current_value['line'])
                     
